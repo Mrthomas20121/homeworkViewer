ef78a50a36b1318cd21980ae4beb0c9e48857bf1
mrthomas20121 <thomas77300@gmail.com>
2019-07-12 09:22:34 +0200

    readme + app.js

---
+++ README.md
@@ -0,0 +1,10 @@
+Create and read Homework!
+
+i made this app to add and read homework because i use a computer at school.
+if i would use a diary, i wouldn't be able to read what i wrote. that's why i made this!
+it was made for people like me that have a desease that disallow them to write properly.
+feel free to use it.
+
+Currently supported language: 
+French By Myself
+English By Myself
---
+++ app.js
@@ -0,0 +1,53 @@
+const {app, dialog, BrowserWindow, Menu} = require('electron')
+const fs = require('fs');
+const path = require('path');
+const isDev = require('electron-is-dev');
+if(isDev) {
+  require('electron-context-menu')({
+    prepend: (params, browserWindow) => []
+  });
+}
+let homework = {
+  label_add_homework:'add homework',
+  label_view_homework:'view homework'
+}
+
+var mainWindow;
+app.on('ready', function() {
+
+  mainWindow = new BrowserWindow({
+    name: "Devoir",
+    width: 720,
+    height: 720,
+    icon: __dirname + '\\app\\icons\\main.png'
+   })
+
+app.on('window-all-closed', function() {
+      app.quit();
+});
+
+app.on('open-file', function (event, file) {
+    var content = fs.readFileSync(file).toString();
+    mainWindow.webContents.send('file-opened', file, content);
+});
+var m = Menu.buildFromTemplate([
+	{
+    label: 'Add homeworks',
+    accelerator: "Ctrl+N",
+      click () { 
+      mainWindow.loadURL('file://' + path.join(__dirname, "app") + '/add.html');
+    }
+  },
+  {
+    label : 'View homeworks',
+    accelerator: "Ctrl+V",
+    click() {
+      if(mainWindow)
+      mainWindow.loadURL('file://' + path.join(__dirname, "app") + '/index.html');
+    }
+  }
+  ])
+mainWindow.setMenu(m);
+mainWindow.loadURL('file://' + path.join(__dirname, "app") + '/index.html');
+
+});
